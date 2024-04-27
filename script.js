b0311df96fdb9e59a840075349446f64bf2babef
Siren <143248933+sirentusk@users.noreply.github.com>
2024-04-27 14:15:54 +1000

    VS Code Edits

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const stickySections = [...document.querySelectorAll('.sticky')]
 
 let images = [
-
-
-
+    'images/quentintarantino.png',
 ]
+
+images.forEach(img => {
+    stickySections.forEach(section => {
+        let image = document.createElement('img');
+        image.src = img;
+        section.querySelector('.scrollsection').appendChild(image)
+    })
+})
+
+window.addEventListener('scroll', (e) => {
+    for(let i = 0; i < stickySections.length; i++){
+        transform(stickySections[i])
+    }
+})
+
+function transform(section){
+    const offsetTop = section.parentElement.offsetTop;
+}
